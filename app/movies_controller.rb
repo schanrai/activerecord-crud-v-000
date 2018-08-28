a0fff69b85139872241551bbd06e94e7362647b8
Sushi Chanrai <sushi@pathworks.co>
2018-08-28 18:31:39 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -97,7 +97,8 @@ end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  __
+  movie = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
+  movie.destroy
   __
 end
 
