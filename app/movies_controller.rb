22cd47df23759c2e8548bbcd704bb9a4c488780e
Sushi Chanrai <sushi@pathworks.co>
2018-08-28 18:16:51 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -70,7 +70,7 @@ def can_find_using_where_clause_and_be_sorted
 end
 
 def can_be_found_updated_and_saved
-  # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
+  # Update the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
   Movie.create(title: 'Awesome Flick')
   movie = Movie.find_by(title: 'Awesome Flick')
   movie.update(title: 'Even Awesomer Flick')
