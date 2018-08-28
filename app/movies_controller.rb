fe587ba530e4866c71447a28f13d13a878cefb8d
Sushi Chanrai <sushi@pathworks.co>
2018-08-28 18:32:40 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -92,7 +92,7 @@ def can_update_multiple_items_at_once
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
   Movie.all.each {|film|
-    film.update(title =>"A Movie")}
+    film.update(title: "A Movie")}
 end
 
 def can_destroy_a_single_item
